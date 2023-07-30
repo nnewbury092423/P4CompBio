a66e201b07352a3d9ab0725b73dc2a3b1c8a4e48
nnewbury092423 <nnewbury9469@gmail.com>
2023-07-30 12:05:36 -0600

    Transfered From Git Classroom

--- msa/todo.py
+++ msa/todo.py
@@ -11,7 +11,7 @@ M62 = [[4, 0, -2, -1, -2, 0, -2, -1, -1, -1, -1, -2, -1, -1, -1, 1, 0, 0, -3, -2
        [-1, -1, -3, -3, 0, -4, -3, 4, -3, 2, 1, -3, -3, -3, -3, -2, -1, 3, -3, -1],
        [-1, -3, -1, 1, -3, -2, -1, -3, 5, -2, -1, 0, -1, 1, 2, 0, -1, -2, -3, -2],
        [-1, -1, -4, -3, 0, -4, -3, 2, -2, 4, 2, -3, -3, -2, -2, -2, -1, 1, -2, -1],
-       [-1, -1, -3, -2, 0, -3, -2, 1, -1, 2, 5, -2, -2, 0, -1, -1, -1, 1, -1, -1],
+       [-1, -1, -3, -2, 0, -3, -2, 1, =a-1, 2, 5, -2, -2, 0, -1, -1, -1, 1, -1, -1],
        [-2, -3, 1, 0, -3, 0, 1, -3, 0, -3, -2, 6, -2, 0, 0, 1, 0, -3, -4, -2],
        [-1, -3, -1, -1, -4, -2, -2, -3, -1, -3, -2, -2, 7, -1, -2, -1, -1, -2, -4, -3],
        [-1, -3, 0, 2, -3, -2, 0, -3, 1, -2, 0, 0, -1, 5, 1, 0, -1, -2, -2, -1],
@@ -45,43 +45,44 @@ def MSA(sequences, tree, indel_rate):
 
 
     def alignment(ntoalign):
-        #  what i L2 is a dicitionary
-        align={}
+
+        # L1 and L2 are lists being aligned
         L1 = aln[ntoalign[0]]
         L2 = aln[ntoalign[1]]
+
+        # S is score matrix
         S = [[None for j in range(len(L2[0])+1)] for i in range(len(L1[0])+1)]
-        # fill in axes
-        #import pdb; pdb.set_trace()
-        # for each letter in first sequence
+
+
+        # fill in axes of svore matrix
         S[0][0] = (0,None)
-        for i in range(1, len(L1[0])+1): # s1 axis
-            #import pdb; pdb.set_trace()
+        for i in range(1, len(L1[0])+1): 
             S[i][0] = (len(L2)*len(L1)*gap*i, 1)
-        for j in range(1, len(L2[0])+1): # s2 axis
+        for j in range(1, len(L2[0])+1): 
             S[0][j] = (len(L2)*len(L2)*gap*j, 2)
         
         
-        # fill in face
-        #import pdb; pdb.set_trace()
+        # fill in full matrix 
+        # iterate through the score matrix i,j
         for i in range(1, len(L1[0])+1):
             for j in range(1, len(L2[0])+1):
-                # add a gap
+                # Calculating total score of added letter requires iterating through each string 
                 blosum = 0
                 for s1 in L1:
                     for s2 in L2:
-                        #import pdb; pdb.set_trace()
                         if s1[i-1]  == '-' and s2[j-1] == '-':
                             blosum = blosum + 0
                         elif  s1[i-1]  == '-' or  s2[j-1] == '-':
                             blosum = blosum + gap
                         else:
                             blosum = blosum + BLOSUM62[s1[i-1]][s2[j-1]]
-                #import pdb; pdb.set_trace()
+                
+                # once we have a score from iterating through each combination of strings, do classic alignment
                 option = [blosum + S[i-1][j-1][0], S[i-1][j][0] + gap*len(L2)*len(L1), S[i][j-1][0] + gap*len(L2)*len(L1)]  
                 max_index = max(enumerate(option), key=lambda x: x[1])[0]
                 arrow = max_index
                 S[i][j] = (max(option), arrow)
-        #import pdb; pdb.set_trace()
+
 
 
         # backtracking adapted from given code:
@@ -92,7 +93,7 @@ def MSA(sequences, tree, indel_rate):
 
         while arrow is not None:
             if arrow == 1:
-                # add gap to L2 strings
+                # add gap to L2 strings and letter to L1
                 num = 0
                 for s1 in L1:  
                     alignL1[num]+=(s1[i-1])
@@ -102,7 +103,7 @@ def MSA(sequences, tree, indel_rate):
                     alignL2[num]+=('-')
                     num += 1
                 i-=1
-                # add gaps to L1 strings
+                # add gaps to L1 strings and letter to L2
             elif arrow == 2:
                 num = 0
                 for s1 in L1:  
@@ -113,7 +114,7 @@ def MSA(sequences, tree, indel_rate):
                     alignL2[num]+=(s2[j-1])
                     num += 1
                 j-=1
-                
+                # add letters to both  L1 and L2
             elif arrow == 0:
                 num = 0
                 for s1 in L1:  
@@ -127,7 +128,8 @@ def MSA(sequences, tree, indel_rate):
                 i-=1
             arrow = S[i][j][1]
 
-        #import pdb; pdb.set_trace()
+
+
         # invert
         num = 0
         for s1 in alignL1:
@@ -137,46 +139,32 @@ def MSA(sequences, tree, indel_rate):
         for s2 in alignL2:
             alignL2[num] = s2[::-1]
             num+=1
-        #import pdb; pdb.set_trace()
-        
-        #import pdb; pdb.set_trace()
 
-        align
         return alignL1 + alignL2
 
 
 
-
+# iterate through the tree
     for node in tree.traverse_postorder():
-        #import pdb; pdb.set_trace()
+
         if node.is_leaf():
-            #import pdb; pdb.set_trace()
             aln[node] = [sequences[node.get_label()]]
-            #print(sequences[node.get_label()])
+
         else:
-            #import pdb; pdb.set_trace()
+            # call alignment on each of the child nodes
             aln[node] = alignment(node.child_nodes())
 
-    # loop through postorder
-    # go from list to dictionary
-    
-    #import pdb; pdb.set_trace()
+
+# convert from list to dictionary
     alinlist = aln[tree.root]
     finalalign = {}
     num = 0
+
+    # iterate nodes in the same order
     for node in tree.traverse_postorder():            
         if node.is_leaf():
+            # if the node is a leaf, t
             finalalign[node.get_label()] = alinlist[num]
             num = num +1
-        #else: 
-         #   import pdb; pdb.set_trace()
-          #  finalalign[node.get_label()] = ''.join([finalalign[child.get_label()] for child in node.child_nodes()] )
 
-
-
-
-
-    #import pdb; pdb.set_trace()
-
-    #raise Exception("NOT IMPLEMENTED ERROR!") # TODO: REPLACE WITH YOUR CODE!
     return finalalign 
